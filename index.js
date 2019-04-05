57fc053e1eeb3fad79ee4e2e6d17df2ca19ff326
Jarek Wojciechowski <jarekwojo@gmail.com>
2019-04-05 16:15:52 -0400

    normalize the movement per step more, and alternate colors

--- index.js
+++ index.js
@@ -4,34 +4,44 @@ var context = canvas.getContext('2d');
 const DPR = window.devicePixelRatio || 1;
 const SIZE = window.innerWidth * .33;
 const MARGIN = SIZE / 20;
+const STEP = SIZE / 20;
 
 canvas.width = SIZE * DPR;
 canvas.height = SIZE * DPR;
 context.scale(DPR, DPR);
-context.lineWidth = 1;
+context.lineWidth = 0;
 
 const startingWidth = canvas.width - MARGIN;
 const startingHeight = canvas.height - MARGIN;
 const MIN_AREA = startingWidth * startingHeight * .01;
 
 
+const getFillStyle = (count) => {
+  return count % 2 == 0 ? '#5C9EAD' : '#EEEEEE';
+}
+
+const getNextDimension = (currentDimension) => {
+  const randomMult = Math.random() / 2 + .75;
+  return currentDimension - (STEP * randomMult);
+}
 
 const drawAndShrink = (currentWidth, currentHeight, count) => {
-  context.strokeRect(MARGIN, MARGIN, currentWidth, currentHeight);
-  const currentArea = currentWidth * currentHeight;
-  const randomMult = Math.random() / 4 + .75;
   var nextWidth, nextHeight;
+  const currentArea = currentWidth * currentHeight;
+
+  context.fillStyle = getFillStyle(count);
+  context.fillRect(MARGIN, MARGIN, currentWidth, currentHeight);
+
   if (currentArea > MIN_AREA) {
     if (Math.random() > .5){
-      nextWidth = currentWidth * randomMult;
+      nextWidth = getNextDimension(currentWidth);
       nextHeight = currentHeight;
     } else {
       nextWidth = currentWidth;
-      nextHeight = currentHeight * randomMult;
+      nextHeight = getNextDimension(currentHeight);
     }
     drawAndShrink(nextWidth, nextHeight, count + 1);
   }
 };
-
 
 drawAndShrink(startingWidth, startingHeight, 0);

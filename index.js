70657c491390bb63309b13b303162248fce8dae3
Jarek Wojciechowski <jarekwojo@gmail.com>
2019-05-20 18:00:38 -0400

    setup the viney grid

--- index.js
+++ index.js
@@ -5,11 +5,18 @@ const DPR = window.devicePixelRatio || 1;
 const SIZE = window.innerWidth * .4;
 const MARGIN = SIZE / 20;
 const STEP = SIZE / 20;
+const TREE_WIDTH = SIZE / 10;
 
 canvas.width = SIZE * DPR;
 canvas.height = SIZE * DPR;
 context.scale(DPR, DPR);
-context.lineWidth = 2;
+context.lineWidth = 3;
+context.lineCap = "square";
+
+// const COLOR1 = "#3D6643";
+// const COLOR2 = "#C1DFD3";
+const COLOR1 = "#404040";
+const COLOR2 = "#FDFDFD";
 
 const rotateDegrees = (context, degrees) => context.rotate(degrees * Math.PI/180);
 
@@ -17,10 +24,10 @@ const getGradient = (x1, y1, x2, y2) => {
   const grd = context.createLinearGradient(x1, y1, x2, y2);
   const modifierLeft = Math.random() * .3;
   const modifierRight = Math.random() * .3;
-  grd.addColorStop(0, "black");
-  grd.addColorStop(.2 + modifierLeft, "white");
-  grd.addColorStop(.8 - modifierRight, "white");
-  grd.addColorStop(1, "black");
+  grd.addColorStop(0, COLOR1);
+  grd.addColorStop(.2 + modifierLeft, COLOR2);
+  grd.addColorStop(.8 - modifierRight, COLOR2);
+  grd.addColorStop(1, COLOR1);
   return grd;
 }
 
@@ -39,65 +46,41 @@ const rotateAndDraw = (degrees, drawFn) => {
   drawFn();
 }
 
-const decideToBranch = (width) => {
-  console.log(width);
-  return width > 2;
-}
-
-const drawBranch = (xStart, yStart, startWidth, length) => {
-  console.log(xStart, 'xStart');
-  console.log(yStart, 'yStart');
-  console.log(startWidth, 'startWidth');
-  console.log(length, 'length');
+const drawBranch = (xStart, yStart, startWidth, rotation) => {
+  context.save();
   context.translate(xStart,yStart);
+  rotateDegrees(context, rotation);
   let rotationAmount = 0;
   let currentWidth = startWidth;
 
-  for(y=0; y < length; y += 1){
-    if(y/length == .25 || y/length == .5 || y/length == .75){
-      if(Math.random() > .8){
-        context.save();
-        rotateDegrees(context, Math.random() * 30 + 15)
-        drawBranch(
-          currentWidth * (Math.random() * .3 + .2),
-          10,
-          currentWidth*.66,
-          length
-        );
-        context.restore();
-      }
-      if(Math.random() > .8){
-        context.save();
-        const degrees = Math.random() * 30 - 45;
-        rotateDegrees(context, degrees);
-        drawBranch(
-          currentWidth * (Math.random() * .3 + .5),
-          10,
-          currentWidth*.66,
-          length
-        );
-        context.restore();
-      }
-    }
+  while(currentWidth >= 0){
     currentWidth -= Math.random() / 6;
     rotationAmount += (Math.random() * 3 - 1.5);
-    rotationAmount = Math.max(rotationAmount, -1.5);
-    rotationAmount = Math.min(rotationAmount, +1.5);
+    rotationAmount = Math.max(rotationAmount, -2.5);
+    rotationAmount = Math.min(rotationAmount, +2.5);
     rotateAndDraw(rotationAmount, () => {
       drawGradientLine(currentWidth/-2, 0, currentWidth/2, 0);
     });
   }
-
-  // if(decideToBranch(currentWidth)){
-  //   context.save();
-  //   rotateDegrees(context, Math.random() * 30 - 15)
-  //   drawBranch(currentWidth/4, 10, currentWidth/2, 75);
-  //   context.restore();
-  //   context.save();
-  //   context.rotate(context, Math.random() * 30 - 15)
-  //   drawBranch(-currentWidth/4,10, currentWidth/2, 75);
-  //   context.restore();
-  // }
+  context.restore();
 }
 
-drawBranch(SIZE/2, SIZE-MARGIN, 60, 120);
+context.beginPath();
+context.fillStyle = COLOR2;
+context.fillRect(0,0,SIZE,SIZE);
+context.stroke();
+
+for(var x1=0; x1<SIZE; x1+=SIZE/10){
+  for(var y1=0; y1<SIZE; y1+=SIZE/10){
+    drawBranch(x1, y1, TREE_WIDTH, Math.random() * 360);
+  }
+}
+
+for(var x2=0; x2<SIZE; x2+=SIZE/10){
+  drawBranch(x2, 0 - SIZE / 10, TREE_WIDTH, 180);
+  drawBranch(x2, SIZE + SIZE/10, TREE_WIDTH, 0);
+}
+for(var y2=0; y2<SIZE; y2+=SIZE/10){
+  drawBranch(0 - SIZE/10   , y2, TREE_WIDTH, 90);
+  drawBranch(SIZE + SIZE/10, y2, TREE_WIDTH, 270);
+}
